75bb834a501897e75090af8e1324fb9a15fd0682
vicpsl <jvboby@gmail.com>
2026-03-09 17:45:20 +0100

    chore: Correct and merge PR 24

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,23 @@
 
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { prettifyError } from "zod";
+import { prettifyError } from 'zod'
 
 import path from 'path'
 
-import { ViteAppConfig } from "./src/schema";
-import { redText } from "./src/composables/utils"
+import { ViteAppConfig } from './src/schema'
+import { redText } from './src/composables/utils'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   // check configuration
-  const parsedEnv = ViteAppConfig.safeParse(env);
+  const parsedEnv = ViteAppConfig.safeParse(env)
   if (!parsedEnv.success) {
     const prettyError = prettifyError(parsedEnv.error);
-    console.error(`\nErrors are found in .env.${mode} file:\n\n`, redText(prettyError), "\n")
-    process.exit(1);
+    console.error(`\nErrors are found in .env.${mode} file:\n\n`, redText(prettyError), '\n')
+    process.exit(1)
   }
 
   return {
